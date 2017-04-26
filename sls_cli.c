e6258125953dc39a261c46761d617d3798727011
Vu Phan <pnvupfiev08@gmail.com>
2017-04-26 23:20:41 +0700

    Add sls_cli.c, udp-echo-server.c
    Integrate CBC with AES 128 bit

---
+++ sls_cli.c
@@ -0,0 +1,249 @@
+/*
+Shell cmd for controlling the SLS
+author Vo Que Son <sonvq@hcmut.edu.vn>
+*/
+ 
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include "aes.h"
+#include "/home/user/contiki/examples/cc2538dk/00_sls/sls.h"
+#define security_en (1)
+#ifdef security_en
+#define CBC 1
+#define ECB 1
+#endif
+// #include "aes.h"
+#define KEY_LENGTH (16)
+#define MAXBUF 100
+// #define MAX_LENGTH 1024
+// #define DELIMS " \t\r\n"
+
+// static    int s_sock;
+static  int     rev_bytes;
+static  struct  sockaddr_in6 rev_sin6;
+static  int     rev_sin6len;
+static  char    rev_buffer[MAXBUF];
+static  int     port;
+static  char    dst_ipv6addr[50];
+static  char    str_port[5];
+static  char    cmd[20];
+static  char    arg[20];
+
+static  cmd_struct_t  tx_cmd, rx_reply;
+static  cmd_struct_t *cmdPtr;
+static  char *p;
+static  uint8_t key[] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
+static  uint8_t iv[]  = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
+
+typedef struct _cbc_param
+{
+  uint8_t in[MAX_CMD_LEN];
+  uint8_t out[MAX_CMD_LEN];
+  uint8_t buffer[MAX_CMD_LEN];
+} cbc_param;
+
+static cbc_param encrypt_param;
+
+/*prototype definition */
+static void print_cmd();
+static void prepare_cmd();
+static void prepare_encrypt();
+static void print_array(char* pstring, int length);
+/*------------------------------------------------*/
+void print_array(char* pstring, int length) {
+int i;
+for(i = 0; i < length; i += 8)
+{
+  // printf("%x\n", (int*)(pstring + i));
+ printf("{%0x, %0x, %0x, %0x, %0x, %0x, %0x}\n", *(int*)(pstring + i), *(int*)(pstring + i + 1), *(int*)(pstring + i + 2), *(int*)(pstring + i + 3), *(int*)(pstring + i + 4), *(int*)(pstring + i + 5), *(int*)(pstring + i + 6), *(int*)(pstring + i + 7));
+}
+}
+void prepare_encrypt() {
+  int cmd_length;
+  int ret;
+  cmd_length = sizeof(tx_cmd);
+  printf("MAX_CMD_LEN = %x, cmd_length = %x\n", MAX_CMD_LEN, cmd_length);
+  memset(&encrypt_param.in, 0, MAX_CMD_LEN);
+  memset(&encrypt_param.out, 0, MAX_CMD_LEN);
+  memset(&encrypt_param.buffer, 0, MAX_CMD_LEN);
+
+  memcpy(&encrypt_param.in, &tx_cmd, cmd_length);
+  printf("tx_cmd:\n");
+  print_array(&tx_cmd, cmd_length);
+
+  printf("encrypt_param.in:\n");
+  print_array(&encrypt_param.in, cmd_length);
+
+  printf("CBC encrypt:\n");
+  AES128_CBC_encrypt_buffer(&encrypt_param.buffer, &encrypt_param.in, cmd_length, &key, &iv);
+  
+  printf("CBD descrypt:\n");
+  // AES128_CBC_decrypt_buffer(&encrypt_param.out, &encrypt_param.buffer, cmd_length, &key, &iv);
+  AES128_CBC_decrypt_buffer(&encrypt_param.out, &encrypt_param.buffer, 16, &key, &iv);
+  AES128_CBC_decrypt_buffer(&encrypt_param.out + 16, &encrypt_param.buffer+ 16, 16, 0, 0);
+
+
+  printf("encrypt_param.buffer:\n");
+  print_array(&encrypt_param.buffer, cmd_length);
+  printf("encrypt_param.out:\n");
+  print_array(&encrypt_param.out, cmd_length);
+  ret = memcmp((int*) &encrypt_param.out, (int*) &encrypt_param.in, cmd_length);
+  if(ret == 0)
+  {
+    printf("SUCCESS!\n");
+  }
+  else
+  {
+    printf(" ret = %x\n", ret);
+ 
+    printf("FAILURE!\n");
+  }
+}
+/*------------------------------------------------*/
+void prepare_cmd() {
+  tx_cmd.sfd = 0x7E;
+  tx_cmd.len = sizeof(tx_cmd);
+  tx_cmd.seq ++;
+  tx_cmd.type = MSG_TYPE_REQ;
+  tx_cmd.err_code = 0;  
+}
+
+
+/*------------------------------------------------*/
+void print_cmd(cmd_struct_t command) {
+  printf("SFD=0x%X; ",command.sfd);
+  printf("len=%d; ",command.len);
+  printf("seq=%d; ",command.seq);
+  printf("type=0x%X; ",command.type);
+  printf("cmd=0x%X; ",command.cmd);
+  printf("err_code=0x%X; ",command.err_code);  
+  printf("arg0=0x%X; ",command.arg[0]);
+  printf("arg1=%d; ",command.arg[1]);
+  printf("arg2=%d; ",command.arg[2]);
+  printf("arg3=%d; ",command.arg[3]);
+  printf("arg4=0x%X;\n",command.arg[4]);
+}
+
+
+
+int main(int argc, char* argv[])
+{
+  int sock;
+  int status;
+  struct addrinfo sainfo, *psinfo;
+  struct sockaddr_in6 sin6;
+  int sin6len;
+  char buffer[MAXBUF];
+  int i;
+
+  sin6len = sizeof(struct sockaddr_in6);
+
+  sprintf(buffer,"led_off");
+	port = 3000;
+  sprintf(dst_ipv6addr,"aaaa::212:7401:1:101");
+	
+  if(argc < 4) {
+    printf("Specify an IPv6 addr or port number or Cmd \n"), exit(1);
+	}
+	else if (argc==4) {
+    sprintf(dst_ipv6addr,argv[1]);      
+    strcpy(str_port,argv[2]);
+    strcpy(cmd,argv[3]);  
+    port = atoi(str_port);
+    sprintf(buffer,cmd);
+    if (strcmp(cmd,"SLS_LED_ON")==0)
+      tx_cmd.cmd = CMD_LED_ON;
+    else if (strcmp(cmd,"CMD_SLS_LED_OFF")==0)
+      tx_cmd.cmd = CMD_LED_OFF;    
+    else if (strcmp(cmd,"SLS_GET_LED_STATUS")==0)
+      tx_cmd.cmd = CMD_GET_LED_STATUS;
+    else if (strcmp(cmd,"SLS_GET_NW_STATUS")==0)
+      tx_cmd.cmd = CMD_GET_NW_STATUS;
+    else if (strcmp(cmd,"SLS_GET_GW_STATUS")==0)
+      tx_cmd.cmd = CMD_GET_GW_STATUS;
+	}		
+	else if (argc==5) {
+    sprintf(dst_ipv6addr,argv[1]);      
+    strcpy(str_port,argv[2]);
+    sprintf(cmd,argv[3]);
+    sprintf(arg,argv[4]);
+		//sprintf(buffer,argv[2]);
+
+    if (strcmp(cmd,"SLS_LED_DIM")==0) {
+      tx_cmd.cmd = CMD_LED_DIM;    
+      tx_cmd.arg[0] = atoi(arg);
+    }
+
+    port = atoi(str_port);
+   	sprintf(buffer,"%s %s",cmd,arg);
+	}	
+  prepare_cmd();
+  //prepare_encrypt();
+
+
+  strtok(buffer, "\n");
+
+  sock = socket(PF_INET6, SOCK_DGRAM,0);
+
+  memset(&sin6, 0, sizeof(struct sockaddr_in6));
+  sin6.sin6_port = htons(port);
+  sin6.sin6_family = AF_INET6;
+  sin6.sin6_addr = in6addr_any;
+
+  status = bind(sock, (struct sockaddr *)&sin6, sin6len);
+
+  if(-1 == status)
+    perror("bind"), exit(1);
+
+  memset(&sainfo, 0, sizeof(struct addrinfo));
+  memset(&sin6, 0, sin6len);
+
+  sainfo.ai_flags = 0;
+  sainfo.ai_family = PF_INET6;
+  sainfo.ai_socktype = SOCK_DGRAM;
+  sainfo.ai_protocol = IPPROTO_UDP;
+  status = getaddrinfo(dst_ipv6addr, str_port, &sainfo, &psinfo);
+
+  //status = sendto(sock, buffer, strlen(buffer), 0,
+  //                   (struct sockaddr *)psinfo->ai_addr, sin6len);
+
+   status = sendto(sock, &tx_cmd, sizeof(tx_cmd), 0,
+                      (struct sockaddr *)psinfo->ai_addr, sin6len);
+  //status = sendto(sock, &encrypt_param.buffer, sizeof(encrypt_param.buffer), 0,
+                   // (struct sockaddr *)psinfo->ai_addr, sin6len);
+
+  printf("Send REQUEST (len=%d) to [%s]:%s\n",status, dst_ipv6addr,str_port);
+  print_cmd(tx_cmd);
+
+  /*wait for a reply */
+	rev_bytes = recvfrom(sock, rev_buffer, MAXBUF, 0,(struct sockaddr *)&rev_sin6, &rev_sin6len);
+	if (rev_bytes<0) {
+    perror("Problem in recvfrom \n");
+    exit(1);
+  }
+  else {
+    printf("Got REPLY (len=%d):\n",rev_bytes);   
+    p = (char *) (&rev_buffer); 
+    cmdPtr = (cmd_struct_t *)p;
+    rx_reply = *cmdPtr;
+    print_cmd(rx_reply);      
+  }
+
+
+
+  shutdown(sock, 2);
+  close(sock); 
+
+   // free memory
+  freeaddrinfo(psinfo);
+  psinfo = NULL;
+  return 0;
+}
+
--- udp-echo-server.c
+++ udp-echo-server.c
@@ -43,75 +43,548 @@
 #include "contiki.h"
 #include "contiki-lib.h"
 #include "contiki-net.h"
-
-#include <string.h>
-
-#define DEBUG DEBUG_PRINT
 #include "net/ip/uip-debug.h"
-#include "dev/watchdog.h"
 #include "dev/leds.h"
 #include "net/rpl/rpl.h"
+#include "dev/watchdog.h"
+#include "dev/uart1.h" 
+#include "sys/rtimer.h"
+#include "dev/rom-util.h"
+#include "dev/cbc.h"
+#include "dev/uart.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "dev/leds.h"
+#include "net/rpl/rpl.h"
+#include "dev/watchdog.h"
+
+#ifdef SLS_USING_CC2538DK
+#include "dev/uart.h"
+#endif
+
+#include "sls.h"	
+
 /*---------------------------------------------------------------------------*/
 #define UIP_IP_BUF   ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
 #define UIP_UDP_BUF  ((struct uip_udp_hdr *)&uip_buf[uip_l2_l3_hdr_len])
 
 #define MAX_PAYLOAD_LEN 120
+#define DEBUG_SIZE 16
+
 /*---------------------------------------------------------------------------*/
 static struct uip_udp_conn *server_conn;
 static char buf[MAX_PAYLOAD_LEN];
 static uint16_t len;
+
+/* SLS define */
+static 	led_struct_t led_db;
+//static struct led_struct_t *led_db_ptr = &led_db;
+
+static 	gw_struct_t gw_db;
+static 	net_struct_t net_db;
+//static struct led_struct_t *gw_db_ptr = &gw_db;
+
+static 	cmd_struct_t cmd, reply;
+//static 	cmd_struct_t *cmdPtr = &cmd;
+
+static 	radio_value_t aux;
+static  char rxbuf[MAX_PAYLOAD_LEN];
+static 	int cmd_cnt;
+static	int	state;
+
+/*define timers */
+static struct uip_udp_conn *client_conn;
+static uip_ipaddr_t server_ipaddr;
+static	struct	etimer	et;
+//static	struct	rtimer	rt;
+static bool	emergency_status;
+
+
+/* define prototype of fucntion call */
+//static 	void set_connection_address(uip_ipaddr_t *ipaddr);
+static 	void get_radio_parameter(void);
+static 	void init_default_parameters(void);
+static 	void reset_parameters(void);
+
+#ifdef SLS_USING_CC2538DK
+static 	unsigned int uart0_send_bytes(const	unsigned  char *s, unsigned int len);
+static 	int uart0_input_byte(unsigned char c);
+//static 	unsigned int uart1_send_bytes(const	unsigned  char *s, unsigned int len);
+//static 	int uart1_input_byte(unsigned char c);
+#define security_enable 1
+#ifdef security_enable
+#define KEY_LENGTH 16
+struct cbc_param
+{
+	bool encrypt;
+	uint8_t key_area;
+	uint8_t key[DEBUG_SIZE];
+	uint8_t iv[DEBUG_SIZE];
+	uint8_t mdata_in[DEBUG_SIZE];
+	uint8_t mdata_out[DEBUG_SIZE];
+	int mdata_len;	
+};
+static struct cbc_param descrypt_param;
+static uint8_t receive_data_flag;
+#endif
+#endif 
+
+static 	void send_cmd_to_led_driver();
+static	void process_hello_cmd(cmd_struct_t command);
+static	void print_cmd_data(cmd_struct_t command);
+static 	void send_reply (cmd_struct_t res);
+static	void blink_led(unsigned char led);
+static void security_prepare();
+static void print_data();
+static void proccess_cmd(void);
+static void get_request_data(void);
+
 /*---------------------------------------------------------------------------*/
 PROCESS(udp_echo_server_process, "UDP echo server process");
 AUTOSTART_PROCESSES(&udp_echo_server_process);
-/*---------------------------------------------------------------------------*/
-static void
-tcpip_handler(void)
-{
-  memset(buf, 0, MAX_PAYLOAD_LEN);
-  if(uip_newdata()) {
-    leds_on(LEDS_RED);
-    leds_on(LEDS_GREEN);
-    len = uip_datalen();
-    memcpy(buf, uip_appdata, len);
- //   PRINTF("%u bytes from [", len);
- //   PRINT6ADDR(&UIP_IP_BUF->srcipaddr);
- //   PRINTF("]:%u\n", UIP_HTONS(UIP_UDP_BUF->srcport));
- //   PRINTF("%s \n",buf);
-    PRINTF("%s",buf);
-    uip_ipaddr_copy(&server_conn->ripaddr, &UIP_IP_BUF->srcipaddr);
-    server_conn->rport = UIP_UDP_BUF->srcport;
 
-    uip_udp_packet_send(server_conn, buf, len);
-    uip_create_unspecified(&server_conn->ripaddr);
-    server_conn->rport = 0;
-  }
-  leds_off(LEDS_RED);
-  leds_off(LEDS_GREEN);
-  return;
+/*---------------------------------------------------------------------------*/
+static void print_data() {
+	uint8_t i;	
+  	PRINTF("data = [");
+	for (i=0;i < DEBUG_SIZE;i++) 
+    	PRINTF("%x,",descrypt_param.mdata_out[i]);
+  	PRINTF("]\n");
 }
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(udp_echo_server_process, ev, data)
+static void security_prepare()
 {
+	int ret;
+	ret = CRYPTO_SUCCESS;
+	static uint8_t key[] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
+	static uint8_t iv[]  = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
+	static uint8_t count = 1; 
+	//static uint8_t data[] = { 0x39, 0xa9, 0xb4, 0x2d, 0xe1, 0x9e, 0x51, 0x2a, 0xb7, 0xf3, 0x04, 0x35, 0x64, 0xc3, 0x51, 0x5a };
+	static uint8_t data[] = {0x73,0xfd,0x7,0xc2,0x8f,0x43,0xb4,0x8d,0xc2,0x4,0xff,0x77,0x59,0x77,0x59,0xaa};
 
-  PROCESS_BEGIN();
-  PRINTF("Starting UDP echo server\n");
+	//static uint8_t data[] = {0x1187e, 0x1000118, 0x10001, 0x00, 0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+	//static uint8_t data[] = {0x7e,0x6b,0x3a,0xb2,0xa4,0xe6,0x90,0x2c,0xf5,0xd7,0x1d,0x2b,0x72,0x56,0x9c,0x54};
+	// descrypt_param.mdata_len = sizeof(cmd_struct_t);
+	descrypt_param.mdata_len = DEBUG_SIZE;
+	descrypt_param.encrypt = false;
+	descrypt_param.key_area = 0;
+	memcpy(&descrypt_param.key, &key, KEY_LENGTH);
+	memcpy(&descrypt_param.iv, &iv, KEY_LENGTH);
+	memcpy(&descrypt_param.mdata_in, &data, descrypt_param.mdata_len);
 
-  server_conn = udp_new(NULL, UIP_HTONS(0), NULL);
-  udp_bind(server_conn, UIP_HTONS(3000));
-
-  PRINTF("Listen port: 3000, TTL=%u\n", server_conn->ttl);
-
-  while(1) {
-    PROCESS_YIELD();
-    if(ev == tcpip_event) {
-      tcpip_handler();
+	ret = aes_load_keys(descrypt_param.key, AES_KEY_STORE_SIZE_KEY_SIZE_128, count, 0);
+    if(ret != CRYPTO_SUCCESS)
+    {
+        printf(" CRYPTO: load key not success\n");
     }
-  }
+}
 
-  PROCESS_END();
+static void process_req_cmd(cmd_struct_t cmd){
+	//uint8_t i;
+	reply = cmd;
+	reply.type =  MSG_TYPE_REP;
+	reply.err_code = ERR_NORMAL;
+
+	if (state==STATE_NORMAL) {
+		switch (cmd.cmd) {
+			case CMD_LED_ON:
+				leds_on(RED);
+				led_db.status = STATUS_LED_ON;
+				//PRINTF ("Execute CMD = %s\n",SLS_LED_ON);
+				break;
+			case CMD_LED_OFF:
+				leds_off(RED);
+				led_db.status = STATUS_LED_OFF;
+				//PRINTF ("Execute CMD = %d\n",CMD_LED_OFF);
+				break;
+			case CMD_LED_DIM:
+				leds_toggle(GREEN);
+				led_db.status = STATUS_LED_DIM;
+				led_db.dim = cmd.arg[0];			
+				//PRINTF ("Execute CMD = %d; value %d\n",CMD_LED_DIM, led_db.dim);
+				break;
+			case CMD_LED_REBOOT:
+				send_reply(reply);
+				clock_delay(5000000);
+				watchdog_reboot();
+				break;
+			case CMD_GET_LED_STATUS:
+				reply.arg[0] = led_db.id;
+				reply.arg[1] = led_db.power;
+				reply.arg[2] = led_db.temperature;
+				reply.arg[3] = led_db.dim; 
+				reply.arg[4] = led_db.status;
+				break;
+			case CMD_GET_NW_STATUS:
+				reply.arg[0] = net_db.channel;
+				reply.arg[1] = net_db.rssi;
+				reply.arg[2] = net_db.lqi;
+				reply.arg[3] = net_db.tx_power; 
+				reply.arg[4] = (net_db.panid >> 8);
+				reply.arg[5] = (net_db.panid) & 0xFF;				
+				break;
+			case CMD_GET_GW_STATUS:
+				break;
+			case CMD_GET_APP_KEY:
+				memcpy(&reply.arg,&net_db.app_code,MAX_CMD_DATA_LEN);
+				break;
+			case CMD_REPAIR_ROUTE:
+				rpl_repair_root(RPL_DEFAULT_INSTANCE);
+				break;
+			default:
+				reply.err_code = ERR_UNKNOWN_CMD;			
+		}
+	}
+	else if (state==STATE_HELLO) {
+		//PRINTF("in HELLO state: no process REQ cmd\n");	
+		switch (cmd.cmd) {
+			case CMD_LED_REBOOT:
+				send_reply(reply);
+				clock_delay(500000);
+				watchdog_reboot();
+				break;
+			case CMD_REPAIR_ROUTE:
+				rpl_repair_root(RPL_DEFAULT_INSTANCE);
+				break;
+			default:
+				break;
+		}		
+		reply = cmd;	
+		reply.err_code = ERR_IN_HELLO_STATE;
+	}
+}
+
+/*---------------------------------------------------------------------------*/
+static void process_hello_cmd(cmd_struct_t command){
+	reply = command;
+	reply.type =  MSG_TYPE_HELLO;
+	reply.err_code = ERR_NORMAL;
+
+	if (state==STATE_HELLO) {
+		switch (command.cmd) {
+			case CMD_LED_HELLO:
+				state = STATE_HELLO;
+				leds_off(LEDS_RED);
+				//rpl_repair_root(RPL_DEFAULT_INSTANCE);
+				break;
+			case CMD_SET_APP_KEY:
+				state = STATE_NORMAL;
+				leds_on(LEDS_RED);
+				memcpy(&net_db.app_code,&cmd.arg,MAX_CMD_DATA_LEN);
+				break;
+			default:
+				reply.err_code = ERR_IN_HELLO_STATE;
+				break;
+		}	
+	}				
+}
+
+/*---------------------------------------------------------------------------*/
+static void print_cmd_data(cmd_struct_t command) {
+	uint8_t i;	
+  	PRINTF("data = [");
+	for (i=0;i<MAX_CMD_DATA_LEN;i++) 
+    	PRINTF("0x%02X,",command.arg[i]);
+  	PRINTF("]\n");
+}
+
+/*---------------------------------------------------------------------------*/
+static void send_reply (cmd_struct_t res) {
+	/* echo back to sender */	
+	//PRINTF("Reply to [");
+	//PRINT6ADDR(&UIP_IP_BUF->srcipaddr);
+	//PRINTF("]:%u %u bytes\n", UIP_HTONS(UIP_UDP_BUF->srcport), sizeof(res));
+	uip_udp_packet_send(server_conn, &res, sizeof(res));
+
+	/* Restore server connection to allow data from any node */
+	uip_create_unspecified(&server_conn->ripaddr);
+	//memset(&server_conn->ripaddr, 0, sizeof(server_conn->ripaddr));
+	//server_conn->rport = 0;
+#ifdef SLS_USING_CC2538DK
+	blink_led(BLUE);
+#else
+	blink_led(RED);	
+#endif	
+}
+
+/*---------------------------------------------------------------------------*/
+/* brief: get data packet */
+static void get_request_data(void)
+{
+	/* initialize flag variable*/
+	receive_data_flag = 0;
+	memset(buf, 0, MAX_PAYLOAD_LEN);
+	if(uip_newdata()) {
+
+	/* mark that data is got */
+	receive_data_flag = 1;
+	len = uip_datalen();
+	memcpy(buf, uip_appdata, len);
+	//PRINTF("Received from [");
+	//PRINT6ADDR(&UIP_IP_BUF->srcipaddr);
+	//PRINTF("]:%u ", UIP_HTONS(UIP_UDP_BUF->srcport));
+	//PRINTF("%u bytes DATA\n",len);
+	
+	uip_ipaddr_copy(&server_conn->ripaddr, &UIP_IP_BUF->srcipaddr);
+	server_conn->rport = UIP_UDP_BUF->srcport;
+
+	get_radio_parameter();
+	reset_parameters();
+	}
 }
 /*---------------------------------------------------------------------------*/
-/**
- * @}
- * @}
- */
+/* brief: proccess cmd data */
+static void proccess_cmd(void)
+{
+	if(receive_data_flag)
+	{
+		cmd = *(cmd_struct_t *)(&buf);
+		PRINTF("Rx Cmd-Struct: sfd=0x%02X; len=%d; seq=%d; type=0x%02X; cmd=0x%02X; err_code=0x%02X\n",cmd.sfd, cmd.len, 
+										cmd.seq, cmd.type, cmd.cmd, cmd.err_code);
+		print_cmd_data(cmd);
+		
+		reply = cmd;		
+		if (cmd.type==MSG_TYPE_REQ) {
+			process_req_cmd(cmd);
+			reply.type = MSG_TYPE_REP;
+		}
+		else if (cmd.type==MSG_TYPE_HELLO) {
+			process_hello_cmd(cmd);	
+			reply.type = MSG_TYPE_HELLO;
+		}
+		else if (cmd.type==MSG_TYPE_EMERGENCY) {
+		}
+
+		//prepare reply and response to sender
+		send_reply(reply);
+
+
+		/* send command to LED-driver */
+		send_cmd_to_led_driver();
+	}
+}
+/*---------------------------------------------------------------------------*/
+static void blink_led(unsigned char led) {
+#ifdef SLS_USING_CC2538DK
+	leds_on(led);
+	clock_delay_usec((uint16_t)2000000);
+	leds_off(led);
+#endif	
+}
+
+#ifdef SLS_USING_CC2538DK
+static int uart0_input_byte(unsigned char c) {
+	if (c==SFD) {
+		cmd_cnt=1;
+		rxbuf[cmd_cnt-1]=c;
+	}
+	else {
+		cmd_cnt++;
+		rxbuf[cmd_cnt-1]=c;
+		if (cmd_cnt==sizeof(cmd_struct_t)) {
+			cmd_cnt=0;
+			PRINTF("Get cmd from LED-driver %s \n",rxbuf);
+			blink_led(BLUE);
+		}
+	}
+	return 1;
+}
+
+static unsigned int uart0_send_bytes(const	unsigned  char *s, unsigned int len) {
+	unsigned int i;
+	for (i = 0; i<len; i++) {
+		uart_write_byte(0, (uint8_t) (*(s+i)));
+   	}   
+   return 1;
+}
+#endif
+
+
+/*---------------------------------------------------------------------------*/
+static void send_cmd_to_led_driver() {
+#ifdef SLS_USING_CC2538DK
+	uart0_send_bytes((const unsigned  char *)(&cmd), sizeof(cmd));	
+#endif
+}
+
+/*---------------------------------------------------------------------------*/
+static void reset_parameters(void) {
+	memset(&reply, 0, sizeof(reply));
+}
+
+/*---------------------------------------------------------------------------*/
+static void get_radio_parameter(void) {
+	NETSTACK_RADIO.get_value(RADIO_PARAM_CHANNEL, &aux);
+	net_db.channel = (unsigned int) aux;
+	//printf("CH: %u ", (unsigned int) aux);	
+
+ 	aux = packetbuf_attr(PACKETBUF_ATTR_RSSI);
+	net_db.rssi = (int8_t)aux;
+ 	//printf("RSSI: %ddBm ", (int8_t)aux);
+
+	aux = packetbuf_attr(PACKETBUF_ATTR_LINK_QUALITY);
+	net_db.lqi = aux;
+ 	//printf("LQI: %u\n", aux);
+
+	NETSTACK_RADIO.get_value(RADIO_PARAM_TXPOWER, &aux);
+	net_db.tx_power = aux;
+ 	//printf("   Tx Power %3d dBm", aux);
+}
+
+/*---------------------------------------------------------------------------*/
+static void init_default_parameters(void) {
+
+	state = STATE_HELLO;
+
+	led_db.id		= 0x20;				//001-0 0000b
+	led_db.panid 	= SLS_PAN_ID;
+	led_db.power	= 120;
+	led_db.dim		= 80;
+	led_db.status	= STATUS_LED_ON; 
+	led_db.temperature = 37;
+
+	gw_db.id		= 0x40;				//010-0 0000b
+	gw_db.panid 	= SLS_PAN_ID;
+	gw_db.power		= 120;
+	gw_db.status	= GW_CONNECTED; 
+
+	cmd.sfd  = SFD;
+	cmd.seq	 = 1;
+	cmd.type = MSG_TYPE_REP;
+	cmd.len  = sizeof(cmd_struct_t);
+
+	net_db.panid 	= SLS_PAN_ID;
+
+	emergency_status = DEFAULT_EMERGENCY_STATUS;
+
+	// init UART0-1
+#ifdef SLS_USING_CC2538DK
+	uart_init(0); 		
+ 	uart_set_input(0,uart0_input_byte);
+#endif
+}
+
+/*---------------------------------------------------------------------------*/
+
+static void set_connection_address(uip_ipaddr_t *ipaddr) {
+  // change this IP address depending on the node that runs the server!
+  uip_ip6addr(ipaddr, 0xaaaa,0x0000,0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0001);
+}
+
+
+/*---------------------------------------------------------------------------*/
+
+static void timeout_hanler(){
+	static int seq_id;
+	char buf[100];
+
+	if (state==STATE_NORMAL) {	
+		if (emergency_status==true) {	
+			sprintf(buf, "Emergency msg %d from the client", ++seq_id);
+			uip_udp_packet_send(client_conn, buf, strlen(buf));
+			PRINTF("Client sending to: ");
+			PRINT6ADDR(&client_conn->ripaddr);
+			PRINTF(" (msg: %s)\n", buf);
+		}
+	}
+}
+
+
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(udp_echo_server_process, ev, data) {
+	int ret, res;
+
+	PROCESS_BEGIN();
+
+   	/* init uart */
+   	uart_init(0);
+   	crypto_init(); 		
+ 	uart_set_input(0,uart0_input_byte);
+
+	// security_prepare();
+
+	// /* start enscrypt data */
+ //    rom_util_memcpy(descrypt_param.mdata_out, descrypt_param.mdata_in, descrypt_param.mdata_len);
+ 
+ //    ret = cbc_crypt_start(descrypt_param.encrypt, descrypt_param.key_area,
+ //                          descrypt_param.iv, descrypt_param.mdata_out, descrypt_param.mdata_out, descrypt_param.mdata_len,
+ //                          &udp_echo_server_process);
+
+ //    if(ret == CRYPTO_SUCCESS)
+ //    {
+ //        PROCESS_WAIT_EVENT_UNTIL((res = cbc_crypt_check_status()) != CRYPTO_PENDING);
+ //        printf("res = %x\n", res);
+ //    }
+ //    if(ret != CRYPTO_SUCCESS)
+ //    {
+ //        PROCESS_PAUSE();
+ //    }
+ //    PROCESS_PAUSE();
+	// print_data();
+
+	//PROCESS_PAUSE();
+	//SENSORS_ACTIVATE(button_sensor);
+
+  	NETSTACK_MAC.off(1);
+
+	init_default_parameters();
+
+	server_conn = udp_new(NULL, UIP_HTONS(0), NULL);
+  	if(server_conn == NULL) {
+    	PROCESS_EXIT();
+  	}
+  	
+  	udp_bind(server_conn, UIP_HTONS(SLS_NORMAL_PORT));
+
+	etimer_set(&et, CLOCK_SECOND*30);
+  	// wait until the timer has expired
+//  	PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
+
+  	set_connection_address(&server_ipaddr);
+	client_conn = udp_new(&server_ipaddr, UIP_HTONS(SLS_EMERGENCY_PORT), NULL);
+
+ 	while(1) {
+    	PROCESS_YIELD();
+    	if(ev == tcpip_event) {
+      		// tcpip_handler();
+
+      		/* get message data from tcpip */
+      		get_request_data();
+
+      		/* initialize descrypt data */
+			security_prepare();
+
+			/* start descrypt data */
+		    rom_util_memcpy(descrypt_param.mdata_out, descrypt_param.mdata_in, descrypt_param.mdata_len);
+		 
+		    ret = cbc_crypt_start(descrypt_param.encrypt, descrypt_param.key_area,
+		                          descrypt_param.iv, descrypt_param.mdata_out, descrypt_param.mdata_out, descrypt_param.mdata_len,
+		                          &udp_echo_server_process);
+
+		    if(ret == CRYPTO_SUCCESS)
+		    {
+		        PROCESS_WAIT_EVENT_UNTIL((res = cbc_crypt_check_status()) != CRYPTO_PENDING);
+		        printf("res = %x\n", res);
+		    }
+		    if(ret != CRYPTO_SUCCESS)
+		    {
+		        PROCESS_PAUSE();
+		    }
+		    PROCESS_PAUSE();
+			print_data();
+
+			/* process cmd data after descrypt data */
+			proccess_cmd();
+    	}
+    	else if (ev==PROCESS_EVENT_TIMER) {
+    		timeout_hanler();
+    		etimer_restart(&et);
+    	}
+    	//else if (ev == sensors_event && data == &button_sensor) {
+      	//	PRINTF("Initiaing global repair\n");
+      	//	rpl_repair_root(RPL_DEFAULT_INSTANCE);
+    	//}
+  	}
+
+	PROCESS_END();
+}
